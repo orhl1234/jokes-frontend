92090476c5cbf1dcfe904ef5d00ac2d87a4e5e68
Or Halachmi <orhalachmi@Ors-MacBook-Air.local>
2024-12-30 01:04:47 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [isLoading, setisLoading] = useState(false)
+  const [joke, setJoke] = useState({})
+  useEffect(() => {
+    async function fetchJokes() {
+      setisLoading(true)
+      const res = await fetch("http://localhost:3000/randomjoke")
+      const data = await res.json()
+      setisLoading(false)
+      if (!isLoading)
+        setJoke(data)
+    }
+    fetchJokes();
+  }, []);
+  
+  if (isLoading) {
+    return <div>Loading...</div>; // Show loading indicator
+  }
+  return (
+    <>
+      {`joke id: ${joke.id} : ${joke.text}`}
+    </>
+  )
+}
+
+export default App
